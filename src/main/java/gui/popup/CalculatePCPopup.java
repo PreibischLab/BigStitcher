f0b02bef6bc47dd29b04555dc40158be71018be2
David Hörl <hoerldavid@gmail.com>
2016-10-19 20:21:51 +0200

    Work-in-progress: GUI will now only display downsampling factors that are already precomputed. TODO: re-add manual selection

--- src/main/java/gui/popup/CalculatePCPopup.java
+++ src/main/java/gui/popup/CalculatePCPopup.java
@@ -2,6 +2,7 @@ package gui.popup;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,6 +10,8 @@ import java.util.List;
 
 import javax.swing.JComponent;
 import javax.swing.JMenuItem;
+
+import com.jgoodies.common.base.Strings;
 
 import algorithm.GroupedViewAggregator;
 import algorithm.GroupedViewAggregator.ActionType;
@@ -28,6 +31,7 @@ import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
 import mpicbg.spim.data.registration.ViewRegistrations;
 import mpicbg.spim.data.sequence.Channel;
 import mpicbg.spim.data.sequence.Illumination;
+import mpicbg.spim.data.sequence.MultiResolutionImgLoader;
 import mpicbg.spim.data.sequence.ViewId;
 import net.imglib2.Dimensions;
 import net.imglib2.realtransform.AbstractTranslation;
@@ -118,31 +122,79 @@ public class CalculatePCPopup extends JMenuItem implements ExplorerWindowSetable
 					boolean is2d = sd.getViewDescriptions().get( gv ).getViewSetup().getSize().numDimensions() == 2;
 					// boolean is2d = false;
 
+					
+										
 					GenericDialog gd = new GenericDialog( "Stitching options" );
 					gd.addChoice( "channel to use", channelNames.toArray( new String[0] ), "average all" );
 					gd.addChoice( "illumination to use", illuminationNames.toArray( new String[0] ), "pick brightest" );
+					
+					
+					// Ask for downsampling factors
+					
+					// we have a HDF5 dataset - display mipmap resolutins present as choices
+					if ( MultiResolutionImgLoader.class.isInstance( sd.getImgLoader() ))
+					{
+						MultiResolutionImgLoader mrImgLoader = (MultiResolutionImgLoader) sd.getImgLoader();
+						// we consider the resolutions present for the first selected ViewId
+						double[][] mipmapResolutions = mrImgLoader.getSetupImgLoader( panel.firstSelectedVD().getViewSetupId()).getMipmapResolutions();
+						String[] dsStrings = new String[mipmapResolutions.length];
+						
+						for (int i = 0; i<mipmapResolutions.length; i++)
+						{
+							String fx = ((Long)Math.round( mipmapResolutions[i][0] )).toString(); 
+							String fy = ((Long)Math.round( mipmapResolutions[i][1] )).toString(); 
+							String fz = ((Long)Math.round( mipmapResolutions[i][2] )).toString();
+							String dsString = String.join( ", ", fx, fy, fz );
+							dsStrings[i] = dsString;
+						}
+						
+						gd.addChoice( "downsampling (x, y, z)", dsStrings, dsStrings[0] );
+						
+					}
+					
+					/*
+					
 					gd.addChoice( "downsample x", ds, ds[0] );
 					gd.addChoice( "downsample y", ds, ds[0] );
 					if ( !is2d )
 					{
 						gd.addChoice( "downsample z", ds, ds[0] );
 					}
+					
+					*/
+					
 					gd.showDialog();
 
 					if ( gd.wasCanceled() )
 						return;
 
 					String channel = gd.getNextChoice();
-
 					String illum = gd.getNextChoice();
+					
 
-					long[] downSamplingFactors = !is2d ? new long[3] : new long[2];
+					//long[] downSamplingFactors = !is2d ? new long[3] : new long[2];
+					long[] downSamplingFactors = new long[] {1, 1, 1};
+					
+					if (( MultiResolutionImgLoader.class.isInstance( sd.getImgLoader() )))
+					{
+						String dsChoice = gd.getNextChoice();
+						String[] choiceSplit = dsChoice.split( ", " );
+						downSamplingFactors[0] = Long.parseLong( choiceSplit[0] );
+						downSamplingFactors[1] = Long.parseLong( choiceSplit[1] );
+						downSamplingFactors[2] = Long.parseLong( choiceSplit[2] );
+					}
+					
+					
+					
+					/*
+		
 					downSamplingFactors[0] = Integer.parseInt( gd.getNextChoice() );
 					downSamplingFactors[1] = Integer.parseInt( gd.getNextChoice() );
 					if ( !is2d )
 					{
 						downSamplingFactors[2] = Integer.parseInt( gd.getNextChoice() );
 					}
+					*/
 
 					PairwiseStitchingParameters params = PairwiseStitchingParameters.askUserForParameters();
 					if ( params == null )
