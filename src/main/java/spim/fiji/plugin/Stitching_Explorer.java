9527f968cd72412b2f1a3c06674da0403d561409
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-08-31 16:14:56 +0200

    update the Stitching_Explorer to the new version of the LoadParseQuery

--- src/main/java/spim/fiji/plugin/Stitching_Explorer.java
+++ src/main/java/spim/fiji/plugin/Stitching_Explorer.java
@@ -1,6 +1,9 @@
 package spim.fiji.plugin;
 
 
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,7 +11,6 @@ import gui.StitchingExplorer;
 import ij.ImageJ;
 import ij.plugin.PlugIn;
 import mpicbg.spim.io.IOFunctions;
-import spim.fiji.ImgLib2Temp.Pair;
 import spim.fiji.plugin.queryXML.GenericLoadParseQueryXML;
 import spim.fiji.plugin.queryXML.LoadParseQueryXML;
 import spim.fiji.spimdata.SpimData2;
@@ -28,46 +30,25 @@ public class Stitching_Explorer implements PlugIn
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				result.setReturnFalse( true );
-				result.getGenericDialog().dispose();
-				setDefineNewDataset();
+				((TextField)result.getGenericDialog().getStringFields().firstElement()).setText( "define" );
+				Button ok = result.getGenericDialog().getButtons()[ 0 ];
+
+				ActionEvent ae =  new ActionEvent( ok, ActionEvent.ACTION_PERFORMED, "");
+				Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(ae);
 			}
 		});
 
 		if ( !result.queryXML( "Stitching Explorer", "", false, false, false, false, false ) && !newDataset )
 			return;
 
-		final SpimData2 data;
-		final String xml;
-		final XmlIoSpimData2 io;
-
-		if ( newDataset )
-		{
-			final Pair< SpimData2, String > dataset = new Define_Multi_View_Dataset().defineDataset( true );
-
-			if ( dataset == null )
-				return;
-
-			data = dataset.getA();
-			xml = dataset.getB();
-			io = new XmlIoSpimData2( "" );
-		}
-		else
-		{
-			data = result.getData();
-			xml = result.getXMLFileName();
-			io = result.getIO();
-		}
+		final SpimData2 data = result.getData();
+		final String xml = result.getXMLFileName();
+		final XmlIoSpimData2 io = result.getIO();;
 
 		final StitchingExplorer< SpimData2, XmlIoSpimData2 > explorer =
 				new StitchingExplorer< SpimData2, XmlIoSpimData2 >( data, xml, io );
 
 		explorer.getFrame().toFront();
-	}
-
-	protected void setDefineNewDataset()
-	{
-		this.newDataset = true;
 	}
 
 	public static void main( String[] args )
