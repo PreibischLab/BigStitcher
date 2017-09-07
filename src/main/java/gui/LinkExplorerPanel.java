PreibischLab/BigStitcher

commit 0b18a0860be3633f2b6d790197c9aacc11443bb2

do not ask a second time if apply&optimize was clicked

src/main/java/gui/StitchingExplorerPanel.java
@@ -266,9 +266,9 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 		}
 		else
 		{
-			boolean doGlobalOpt = false;
-			if (savedFilteringAndGrouping != null)
-				doGlobalOpt = JOptionPane.showConfirmDialog( linkFrame, "Proceed to Global Optimization?", "Optimize Globally?", JOptionPane.YES_NO_OPTION ) == JOptionPane.YES_OPTION;
+			boolean doGlobalOpt = true;
+//			if (savedFilteringAndGrouping != null)
+//				doGlobalOpt = JOptionPane.showConfirmDialog( linkFrame, "Proceed to Global Optimization?", "Optimize Globally?", JOptionPane.YES_NO_OPTION ) == JOptionPane.YES_OPTION;
 
 			quitLinkExplorer();
 			linkOverlay.clearActiveLinks();

commit c6cf5be5fdc2eb499f44a6d56a4edf4ab9531469

restore previous selection in stitching explorer upon leaving link preview also fix a bug introduced in 0b18a0860be3633f2b6d790197c9aacc11443bb2

src/main/java/gui/LinkExplorerPanel.java
@@ -259,8 +259,7 @@ public class LinkExplorerPanel extends JPanel implements SelectedViewDescription
 
 		cancelButton.addActionListener( ev -> 
 		{
-			parent.setSavedFilteringAndGrouping( null );
-			parent.togglePreviewMode();
+			parent.togglePreviewMode(false);
 		});
 
 		applyButton.addActionListener( ev -> {
@@ -270,8 +269,7 @@ public class LinkExplorerPanel extends JPanel implements SelectedViewDescription
 			final List< ? extends Pair< Group< ? extends ViewId >, Group< ? extends ViewId > > > pairs = 
 					(List< ? extends Pair< Group< ? extends ViewId >, Group< ? extends ViewId > > >) parent.getSavedFilteringAndGrouping().getComparisons();
 			model.getFilteredResults().applyToWrappedSubset( (Collection< Pair< Group< ViewId >, Group< ViewId > > >) pairs );
-			parent.setSavedFilteringAndGrouping( null );
-			parent.togglePreviewMode();
+			parent.togglePreviewMode(false);
 		});
 
 		applyAndRunButton.addActionListener( ev -> {
@@ -281,7 +279,7 @@ public class LinkExplorerPanel extends JPanel implements SelectedViewDescription
 			final List< ? extends Pair< Group< ? extends ViewId >, Group< ? extends ViewId > > > pairs = 
 					(List< ? extends Pair< Group< ? extends ViewId >, Group< ? extends ViewId > > >) parent.getSavedFilteringAndGrouping().getComparisons();
 			model.getFilteredResults().applyToWrappedSubset( (Collection< Pair< Group< ViewId >, Group< ViewId > > >) pairs );
-			parent.togglePreviewMode();
+			parent.togglePreviewMode(true);
 		});
 
 		buttons.add( applyButton );

src/main/java/gui/StitchingExplorerPanel.java
@@ -233,7 +233,31 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 		}
 	}
 
-	public void togglePreviewMode()
+	private void restoreSelection()
+	{
+		if (savedFilteringAndGrouping == null)
+			return;
+
+		final List< Group< BasicViewDescription< ? > > > oldGroups = savedFilteringAndGrouping.getGroupedViews( true );
+		final List< List< BasicViewDescription< ? > > > allGroups = tableModel.getElements();
+
+		for (int i = 0; i<allGroups.size(); i++)
+		{
+			final ArrayList< BasicViewDescription< ? > > uiGroup = new ArrayList<>(allGroups.get( i ));
+			SpimData2.filterMissingViews( getSpimData(), uiGroup );
+			boolean inOldSelection = false;
+			for (final Group< BasicViewDescription< ? > > oldGroup : oldGroups)
+				if (oldGroup.getViews().containsAll( uiGroup ))
+				{
+					inOldSelection = true;
+					break;
+				}
+			if (inOldSelection)
+				table.getSelectionModel().addSelectionInterval( i, i );
+		}
+	}
+
+	public void togglePreviewMode(boolean doGlobalOpt)
 	{
 		previewMode = !previewMode;
 		linkOverlay.isActive = previewMode;
@@ -266,7 +290,6 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 		}
 		else
 		{
-			boolean doGlobalOpt = true;
 //			if (savedFilteringAndGrouping != null)
 //				doGlobalOpt = JOptionPane.showConfirmDialog( linkFrame, "Proceed to Global Optimization?", "Optimize Globally?", JOptionPane.YES_NO_OPTION ) == JOptionPane.YES_OPTION;
 
@@ -286,6 +309,8 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 					colorSources(bdvPopup().bdv.getSetupAssignments().getConverterSetups(), colorOffset );
 				}
 			}
+
+			restoreSelection();
 
 			if (doGlobalOpt)
 			{
@@ -733,7 +758,7 @@ public class StitchingExplorerPanel<AS extends AbstractSpimData< ? >, X extends 
 			public void windowClosing(WindowEvent evt)
 			{
 				setSavedFilteringAndGrouping( null );
-				togglePreviewMode();
+				togglePreviewMode(false);
 			}
 		} );
 

src/main/java/gui/popup/CalculatePCPopup.java
@@ -177,7 +177,7 @@ public class CalculatePCPopup extends JMenuItem implements ExplorerWindowSetable
 							if (choice == JOptionPane.YES_OPTION)
 							{
 								((StitchingExplorerPanel< ?, ? >) panel).setSavedFilteringAndGrouping( filteringAndGrouping );
-								((StitchingExplorerPanel< ?, ? >) panel).togglePreviewMode();
+								((StitchingExplorerPanel< ?, ? >) panel).togglePreviewMode(false);
 							}
 						}
 					}

src/main/java/gui/popup/PairwiseInterestPointRegistrationPopup.java
@@ -231,7 +231,7 @@ public class PairwiseInterestPointRegistrationPopup extends JMenu implements Exp
 							if (choice == JOptionPane.YES_OPTION)
 							{
 								((StitchingExplorerPanel< ?, ? >) panel).setSavedFilteringAndGrouping( filteringAndGrouping );
-								((StitchingExplorerPanel< ?, ? >) panel).togglePreviewMode();
+								((StitchingExplorerPanel< ?, ? >) panel).togglePreviewMode(false);
 							}
 						}
 					}

src/main/java/gui/popup/TogglePreviewPopup.java
@@ -43,7 +43,7 @@ public class TogglePreviewPopup extends JMenuItem implements ExplorerWindowSetab
 				return;
 			}
 
-			((StitchingExplorerPanel< ?, ? >) panel).togglePreviewMode();	
+			((StitchingExplorerPanel< ?, ? >) panel).togglePreviewMode(false);
 			
 		}
 	}