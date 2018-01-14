32300519f6f6cd83203dbfc4685a03919a029f20
Stephan Preibisch <stephan.preibisch@gmx.de>
2018-01-14 14:16:05 +0100

    made an extra class for running the global opt from within the GUI to be able to run it more easily and not parallel

---
+++ src/main/java/net/preibisch/stitcher/algorithm/globalopt/ExecuteGlobalOpt.java
@@ -0,0 +1,126 @@
+package net.preibisch.stitcher.algorithm.globalopt;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import mpicbg.spim.data.generic.AbstractSpimData;
+import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
+import mpicbg.spim.data.sequence.Channel;
+import mpicbg.spim.data.sequence.Illumination;
+import mpicbg.spim.data.sequence.Tile;
+import mpicbg.spim.data.sequence.ViewId;
+import mpicbg.spim.io.IOFunctions;
+import net.imglib2.util.Pair;
+import net.preibisch.mvrecon.fiji.spimdata.SpimData2;
+import net.preibisch.mvrecon.fiji.spimdata.explorer.ExplorerWindow;
+import net.preibisch.mvrecon.fiji.spimdata.explorer.FilteredAndGroupedExplorerPanel;
+import net.preibisch.mvrecon.process.interestpointregistration.pairwise.constellation.grouping.Group;
+import net.preibisch.stitcher.algorithm.SpimDataFilteringAndGrouping;
+import net.preibisch.stitcher.gui.StitchingExplorerPanel;
+import net.preibisch.stitcher.gui.overlay.DemoLinkOverlay;
+
+public class ExecuteGlobalOpt implements Runnable
+{
+	private ExplorerWindow< ? extends AbstractSpimData< ? extends AbstractSequenceDescription< ?, ?, ? > >, ? > panel;
+	private boolean expertMode;
+
+	public ExecuteGlobalOpt(
+			final ExplorerWindow< ? extends AbstractSpimData< ? extends AbstractSequenceDescription< ?, ?, ? > >, ? > panel, 
+			final boolean expertMode )
+	{
+		this.panel = panel;
+		this.expertMode = expertMode;
+	}
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			if (!SpimData2.class.isInstance( panel.getSpimData() ) )
+			{
+				IOFunctions.println(new Date( System.currentTimeMillis() ) + "ERROR: expected SpimData2, but got " + panel.getSpimData().getClass().getSimpleName());
+				return;
+			}
+
+			final GlobalOptimizationParameters params = expertMode ? GlobalOptimizationParameters.askUserForParameters() : new GlobalOptimizationParameters();
+			if ( params == null )
+				return;
+
+			final SpimDataFilteringAndGrouping< SpimData2 > filteringAndGrouping;
+			final boolean isSavedFaG = ( ( (StitchingExplorerPanel< ?, ? >) panel ).getSavedFilteringAndGrouping() != null );
+			if ( !isSavedFaG )
+			{
+				FilteredAndGroupedExplorerPanel< SpimData2, ? > panelFG = (FilteredAndGroupedExplorerPanel< SpimData2, ? >) panel;
+				filteringAndGrouping = new SpimDataFilteringAndGrouping< SpimData2 >(
+						(SpimData2) panel.getSpimData() );
+
+				if (expertMode)
+				{
+					filteringAndGrouping.askUserForFiltering( panelFG );
+					if ( filteringAndGrouping.getDialogWasCancelled() )
+						return;
+
+					filteringAndGrouping.askUserForGrouping( panelFG );
+					if ( filteringAndGrouping.getDialogWasCancelled() )
+						return;
+				}
+				else
+				{
+					// use whatever is selected in panel as filters
+					filteringAndGrouping.addFilters( panelFG.selectedRowsGroups().stream().reduce( new ArrayList<>(), (x,y ) -> {x.addAll( y ); return x;}) );
+
+					// get the grouping from panel and compare Tiles
+					panelFG.getTableModel().getGroupingFactors().forEach( g -> filteringAndGrouping.addGroupingFactor( g ));
+					filteringAndGrouping.addComparisonAxis( Tile.class );
+
+					// compare by Channel if channels were ungrouped in UI
+					if (!panelFG.getTableModel().getGroupingFactors().contains( Channel.class ))
+						filteringAndGrouping.addComparisonAxis( Channel.class );
+
+					// compare by Illumination if illums were ungrouped in UI
+					if (!panelFG.getTableModel().getGroupingFactors().contains( Illumination.class ))
+						filteringAndGrouping.addComparisonAxis( Illumination.class );
+
+				}
+			}
+			else
+			{
+				// FIXME: there is some generics work to be done,
+				// obviously
+				filteringAndGrouping = (SpimDataFilteringAndGrouping< SpimData2 >) ( (StitchingExplorerPanel< ?, ? >) panel ).getSavedFilteringAndGrouping();
+			}
+
+			final ArrayList< Pair< Group< ViewId >, Group< ViewId > > > removedInconsistentPairs = new ArrayList<>();
+
+			GlobalOptStitcher.processGlobalOptimization( (SpimData2) panel.getSpimData(), filteringAndGrouping, params, removedInconsistentPairs, !expertMode );
+
+			GlobalOptStitcher.removeInconsistentLinks( removedInconsistentPairs, ((SpimData2) panel.getSpimData()).getStitchingResults().getPairwiseResults() );
+
+			final DemoLinkOverlay demoOverlay;
+
+			if ( !StitchingExplorerPanel.class.isInstance( panel ) )
+				demoOverlay = null;
+			else
+				demoOverlay = (( StitchingExplorerPanel<?,?> )panel).getDemoLinkOverlay();
+
+			if ( demoOverlay != null )
+			{
+				synchronized ( demoOverlay )
+				{
+					demoOverlay.getInconsistentResults().clear();
+					demoOverlay.getInconsistentResults().addAll( removedInconsistentPairs );
+				}
+			}
+		}
+		finally
+		{
+			System.out.println( "resetting savedFilteringAndGrouping" );
+			// remove saved filtering and grouping once we are done here
+			// regardless of whether optimization was successful or not
+			( (StitchingExplorerPanel< ?, ? >) panel ).setSavedFilteringAndGrouping( null );
+		}
+
+		panel.bdvPopup().updateBDV();
+	}
+}
