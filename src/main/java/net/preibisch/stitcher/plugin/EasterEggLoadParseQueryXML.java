1682913dd0a4720baa63d0cd81356e20fcb6ac42
Stephan Preibisch <stephan.preibisch@gmx.de>
2023-07-13 09:41:00 -0400

    show simulated shortcuts

--- src/main/java/net/preibisch/stitcher/plugin/EasterEggLoadParseQueryXML.java
+++ src/main/java/net/preibisch/stitcher/plugin/EasterEggLoadParseQueryXML.java
@@ -105,6 +105,7 @@ public class EasterEggLoadParseQueryXML extends LoadParseQueryXML
 		}
 		else
 		{
+			this.message2 = "To access simulated datasets type 'fractal' or 'beads'";
 			return super.tryParsing( xmlfile, parseAllTypes );
 		}
 	}
